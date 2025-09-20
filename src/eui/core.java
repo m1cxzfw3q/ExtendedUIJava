75b7cc7825568510c3a112167ab97598d46b3f4f
m1cxzfw3q <mcxzfwq@outlook.com>
2025-09-20 15:17:38 +0800

    test

--- src/eui/core.java
+++ src/eui/core.java
@@ -8,10 +8,17 @@ import arc.Events;
 import mindustry.Vars;
 
 public class core extends Mod {
+    public boolean isAutoGG = false;
     public core() {
         Events.run(EventType.Trigger.class, () -> {
             if ((Vars.state.teams.cores(Vars.player.team()).size == 0 || Vars.state.rules.canGameOver) &&
-                    Core.settings.getBool("eui-AlertMarker", true)) Call.sendChatMessage("gg");
+                    Core.settings.getBool("eui-AlertMarker", true) && !isAutoGG) {
+                Call.sendChatMessage("gg");
+                isAutoGG = true;
+            }
+        });
+        Events.on(EventType.WorldLoadEvent.class, e -> {
+            isAutoGG = false;
         });
     }
 
